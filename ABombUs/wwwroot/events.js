60142d9283d455ff18c2461a0557afc315672bff
iWilli26 <willi.nguyen@hotmail.fr>
2022-12-19 13:04:18 +0100

    resize fixed, mouse down done :sparkles:

--- ABombUs/wwwroot/events.js
+++ ABombUs/wwwroot/events.js
@@ -3,8 +3,12 @@ $(document).ready(function () {
     //onmousedown use the sprite
 
     $(canvas).on('click', function (e) {
-        let x = Math.floor(parseInt(e.clientX - canvasx) / 40)
-        let y = Math.floor(parseInt(e.clientY - canvasy) / 40)
+        let x = Math.floor(
+            parseInt(e.clientX - canvasx) / (canvas.width / width)
+        )
+        let y = Math.floor(
+            parseInt(e.clientY - canvasy) / (canvas.height / height)
+        )
 
         if (board.some((x) => x.some((y) => y.Item2))) {
             //If Flagged
@@ -21,8 +25,12 @@ $(document).ready(function () {
     })
 
     $(canvas).on('contextmenu', function (e) {
-        let x = Math.floor(parseInt(e.clientX - canvasx) / 40)
-        let y = Math.floor(parseInt(e.clientY - canvasy) / 40)
+        let x = Math.floor(
+            parseInt(e.clientX - canvasx) / (canvas.width / width)
+        )
+        let y = Math.floor(
+            parseInt(e.clientY - canvasy) / (canvas.height / height)
+        )
 
         //If Visible
         if (board[x][y].Item2) {
@@ -35,4 +43,81 @@ $(document).ready(function () {
     $(canvas).on('mousemove', function (e) {
         connection.invoke('MouseMove', e.clientX, e.clientY)
     })
+    $(canvas)
+        .mousedown(function (e) {
+            handleMouseDown(e)
+            $(this).mousemove(function (e) {
+                updateBoard([], [])
+                handleMouseDown(e)
+            })
+        })
+        .mouseup(function () {
+            $(this).unbind('mousemove')
+        })
+        .mouseout(function () {
+            $(this).unbind('mousemove')
+        })
+    //onmousedown change the sprite, onmouseup change the sprite back
+    // $(canvas).on('mousedown', function (e) {
+    //     let x = Math.floor(
+    //         parseInt(e.clientX - canvasx) / (canvas.width / width)
+    //     )
+    //     let y = Math.floor(
+    //         parseInt(e.clientY - canvasy) / (canvas.height / height)
+    //     )
+    //     if (board[x][y].Item2) {
+    //         if (board.some((x) => x.some((y) => y.Item2))) {
+    //             //change sprite of all tiles non visible or flagged around the clicked tile to 16,0
+    //             for (let i = x - 1; i <= x + 1; i++) {
+    //                 for (let j = y - 1; j <= y + 1; j++) {
+    //                     if (i < 0 || i >= width || j < 0 || j >= height) {
+    //                         continue
+    //                     }
+    //                     if (!board[i][j].Item2 && !board[i][j].Item3) {
+    //                         drawSprite(16, 0, i, j)
+    //                     }
+    //                 }
+    //             }
+    //         } else {
+    //             drawSprite(16, 0, x, y)
+    //         }
+    //     }
+    // })
+    // $(canvas).on('mouseup', function (e) {
+    //     let x = Math.floor(
+    //         parseInt(e.clientX - canvasx) / (canvas.width / width)
+    //     )
+    //     let y = Math.floor(
+    //         parseInt(e.clientY - canvasy) / (canvas.height / height)
+    //     )
+    //     if (board.some((x) => x.some((y) => y.Item2))) {
+    //         return
+    //     }
+    //     drawSprite(0, 0, x, y)
+    // })
+
+    // //on hover draw a rectangle around the tile and delete it on mouseout
+    // $(canvas).on('hover', function (e) {
+    //     let x = Math.floor(
+    //         parseInt(e.clientX - canvasx) / (canvas.width / width)
+    //     )
+    //     let y = Math.floor(
+    //         parseInt(e.clientY - canvasy) / (canvas.height / height)
+    //     )
+    //     ctx.strokeRect(
+    //         (x * canvas.width) / width,
+    //         (y * canvas.height) / height,
+    //         canvas.width / width,
+    //         canvas.height / height
+    //     )
+    // })
+    // $(canvas).on('mouseout', function (e) {
+    //     let x = Math.floor(
+    //         parseInt(e.clientX - canvasx) / (canvas.width / width)
+    //     )
+    //     let y = Math.floor(
+    //         parseInt(e.clientY - canvasy) / (canvas.height / height)
+    //     )
+    //     drawSprite(0, 0, x, y)
+    // })
 })
--- ABombUs/wwwroot/render.js
+++ ABombUs/wwwroot/render.js
@@ -7,13 +7,42 @@ const height = 15
 
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
+
+// Runs each time the DOM window resize event fires.
+// Resets the canvas dimensions to match window,
+// then draws the new borders accordingly.
+
 let canvasx = $(canvas).offset().left
 let canvasy = $(canvas).offset().top
 
 const drawSprite = (dx, dy, i, j) =>
-    ctx.drawImage(spriteimage, dx, dy, 16, 16, i * 40, j * 40, 40, 40)
+    ctx.drawImage(
+        spriteimage,
+        dx,
+        dy,
+        16,
+        16,
+        (i * canvas.width) / 30,
+        j * (canvas.height / 15),
+        canvas.width / 30,
+        canvas.height / 15
+    )
 
-const updateBoard = (explodedMines, wrongMines) => {
+function drawBoard() {
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            drawSprite(0, 0, i, j)
+        }
+    }
+}
+drawBoard()
+
+const clearBoard = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    drawBoard()
+}
+
+const updateBoard = (explodedMine) => {
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             if (board[i][j].Item2) {
@@ -57,14 +86,51 @@ const updateBoard = (explodedMines, wrongMines) => {
             }
         }
     }
-    if (explodedMines.length > 0) {
-        explodedMines.forEach((mine) => {
-            drawSprite(96, 0, mine.Item1, mine.Item2)
-        })
-        if (wrongMines.length > 0) {
-            wrongMines.forEach((mine) => {
-                drawSprite(112, 0, mine.Item1, mine.Item2)
-            })
+    if (explodedMine.length > 0) {
+        console.log(explodedMine)
+        let audio = new Audio('./vine-boom.mp3')
+        audio.volume = 1
+        audio.play()
+        for (let i = 0; i < explodedMine.length; i++) {
+            drawSprite(96, 0, explodedMine[i].Item1, explodedMine[i].Item2)
         }
+    }
+}
+
+function initialize() {
+    window.addEventListener('resize', resizeCanvas, false)
+    resizeCanvas()
+}
+function resizeCanvas() {
+    canvas.width = Math.min(window.innerWidth, 1244)
+    canvas.height = canvas.width / 2
+    console.log(canvas.width, canvas.height)
+    updateBoard([], [])
+}
+
+initialize()
+
+const handleMouseDown = (e) => {
+    let x = Math.floor(parseInt(e.clientX - canvasx) / (canvas.width / width))
+    let y = Math.floor(parseInt(e.clientY - canvasy) / (canvas.height / height))
+    if (board[x][y].Item3) {
+        return
+    }
+    if (board[x][y].Item2) {
+        if (board.some((x) => x.some((y) => y.Item2))) {
+            //change sprite of all tiles non visible or flagged around the clicked tile to 16,0
+            for (let i = x - 1; i <= x + 1; i++) {
+                for (let j = y - 1; j <= y + 1; j++) {
+                    if (i < 0 || i >= width || j < 0 || j >= height) {
+                        continue
+                    }
+                    if (!board[i][j].Item2 && !board[i][j].Item3) {
+                        drawSprite(16, 0, i, j)
+                    }
+                }
+            }
+        }
+    } else {
+        drawSprite(16, 0, x, y)
     }
 }
